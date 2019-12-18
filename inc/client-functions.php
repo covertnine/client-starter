96de41ebd3aee4c8c0242c8bf6b8e1f62382f740
COVERT NINE <tim@covertnine.com>
2019-12-17 18:20:22 -0600

    updating client boilerplate to match up with c9 theme for asset enqueuing

--- inc/client-functions.php
+++ inc/client-functions.php
@@ -17,7 +17,6 @@ if ( ! function_exists( 'client_scripts' ) ) {
 	function client_scripts() {
 
 		wp_enqueue_style( 'client-styles', get_template_directory_uri() . '/client/client-assets/dist/client.min.css', array( 'c9-styles' ) );
-		wp_enqueue_style( 'client-styles', get_template_directory_uri() . '/client/client-assets/dist/client-editor.min.css', 99999999 );
 		wp_enqueue_script( 'client-scripts', get_template_directory_uri() . '/client/client-assets/custom-client.js', array( 'jquery', 'smooth-state' ), true );
 
 		//some examples of extending scripts
@@ -26,15 +25,15 @@ if ( ! function_exists( 'client_scripts' ) ) {
 
 	}
 } // endif function_exists( 'client_scripts' ).
-add_action( 'wp_enqueue_scripts', 'client_scripts', 2 );
+add_action( 'wp_enqueue_scripts', 'client_scripts', 20 );
 
 if ( ! function_exists( 'c9_client_editor_style' ) ) {
 	/**
 	 * Add client compiled files to gutenberg editor.
 	 */
 	function c9_client_editor_style() {
-		wp_enqueue_style( 'c9-client-styles', get_template_directory_uri() . '/client/client-assets/dist/client.css' );
-		wp_enqueue_style( 'c9-client-editor-styles', get_template_directory_uri() . '/client/client-assets/dist/client-editor.css' );
+		wp_enqueue_style( 'c9-client-styles', get_template_directory_uri() . '/client/client-assets/dist/client.min.css' );
+		wp_enqueue_style( 'c9-client-editor-styles', get_template_directory_uri() . '/client/client-assets/dist/client-editor.min.css' );
 	}
 	add_action( 'enqueue_block_editor_assets', 'c9_client_editor_style', 99999999 );
 } //end if function exists
