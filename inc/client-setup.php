98f41d6fac613ce14ab5f0b874436b4bd8bff1d5
tim <tim@covertnine.com>
2020-08-09 12:11:04 -0500

    organizing code and optimizing

--- inc/client-setup.php
+++ inc/client-setup.php
@@ -25,7 +25,7 @@ if (!function_exists('c9_client_setup')) {
 		}
 
 		/**
-		 * Editor color palette
+		 * Apearance > Customizer for fresh sites
 		 * Customizer Sample Content
 		 */
 		add_theme_support(
@@ -39,43 +39,40 @@ if (!function_exists('c9_client_setup')) {
 					'about'			=> array(
 						'comment_status'	=> 'closed',
 						'post_type'			=> 'page',
-						'post_title'		=> __('Our History', 'c9-work'),
+						'post_title'		=> __('Our History', 'c9-starter'),
 						'post_content'		=>  $wp_filesystem->get_contents(get_template_directory_uri() . '/client/content/about.html')
 					),
 					'services'		=> array(
 						'comment_status'	=> 'closed',
 						'post_type'			=> 'page',
-						'post_title'		=> __('Services', 'c9-work'),
+						'post_title'		=> __('Services', 'c9-starter'),
 						'post_content'		=>  $wp_filesystem->get_contents(get_template_directory_uri() . '/client/content/services.html')
 					),
 					'styleguide'		=> array(
 						'comment_status'	=> 'closed',
 						'post_type'			=> 'page',
-						'post_title'		=> __('Style Guide', 'c9-work'),
+						'post_title'		=> __('Style Guide', 'c9-starter'),
 						'post_content'		=>  $wp_filesystem->get_contents(get_template_directory_uri() . '/client/content/styleguide.html')
 					),
 					'contact'		=> array(
 						'comment_status'	=> 'closed',
 						'post_content'		=>  $wp_filesystem->get_contents(get_template_directory_uri() . '/client/content/contact.html')
 					),
-					'news'			=> array(
-						'post_content'			=> __('This page will show all of the news posts once you have populated your database with news items.', 'c9-work')
+					'blog'			=> array(
+						'post_content'			=> __('This page will show all of the blog posts once you have populated your database with blog items.', 'c9-starter')
 					),
 				),
 				'options'			=> array(
 					'show_on_front'		=> 'page',
 					'page_on_front'		=> '{{home}}',
-					'page_for_posts' 	=> '{{news}}',
-					'blogname'			=> 'C9 Work',
-					'blogdescription'	=> __('A WordPress Theme for small businesses with WooCommerce product catalogs, forms, and mega menu support.', 'c9-work'),
+					'page_for_posts' 	=> '{{blog}}',
+					'blogname'			=> 'C9 Starter',
+					'blogdescription'	=> __('A blocks-based WordPress Theme starter theme for musicians, events, small businesses, restaurants, dispensaries, and bloggers.', 'c9-starter'),
 				),
 				'theme_mods'		=> array(
 					'c9_show_search'		=> 'show',
 					'c9_copyright_content'	=> '&copy; 2020. <a href="https://www.covertnine.com" title="Web design company in Chicago" target="_blank">WordPress Website design by COVERT NINE</a>.',
-					'c9_default_font'		=> 'yes',
-					'c9_heading_font'		=> 'Bebas Neue',
-					'c9_subheading_font'	=> 'Lobster',
-					'c9_body_font'			=> '',
+					'c9_default_font'		=> 'no',
 					'c9_author_visible'		=> 'hide',
 					'c9_blog_sidebar'		=> 'hide',
 					'c9_archive_sidebar'	=> 'hide'
@@ -83,7 +80,7 @@ if (!function_exists('c9_client_setup')) {
 				),
 				'nav_menus'		=> array(
 					'primary'		=> array(
-						'name'			=>	__('Top Navigation Menu', 'c9-work'),
+						'name'			=>	__('Top Navigation Menu', 'c9-starter'),
 						'items'			=> array(
 							'page_home',
 							'page_about'	=> array(
@@ -101,23 +98,23 @@ if (!function_exists('c9_client_setup')) {
 								'object'	=> 'page',
 								'object_id'	=> '{{styleguide}}',
 							),
-							'page_news',
+							'page_blog',
 							'page_contact',
 						),
 					),
 				),
 				'widgets'	=> array(
 					'footerfull'	=> array(
-						'c9work_resources'	=> array(
+						'c9starter_resources'	=> array(
 							'text',
 							array(
-								'title'	=> __('Resources Menu', 'c9-work'),
+								'title'	=> __('Resources Menu', 'c9-starter'),
 								'text'	=> '<ul id="menu-footer-resources" class="menu">
 									<li class="menu-item">
 										<a href="#">About</a>
 									</li>
 									<li class="menu-item">
-										<a href="#">News</a>
+										<a href="#">blog</a>
 									</li>
 									<li class="menu-item">
 										<a href="#">Library</a>
@@ -131,13 +128,13 @@ if (!function_exists('c9_client_setup')) {
 								</ul>'
 							)
 						),
-						'c9work_company'	=> array(
+						'c9starter_company'	=> array(
 							'text',
 							array(
-								'title'	=> __('Company Menu', 'c9-work'),
+								'title'	=> __('Company Menu', 'c9-starter'),
 								'text'	=> '<ul id="menu-footer-company" class="menu">
 									<li class="menu-item">
-										<a href="#">Catalog</a>
+										<a href="#">Our History</a>
 									</li>
 									<li class="menu-item">
 										<a href="#">Corporate Governance</a>
@@ -155,22 +152,22 @@ if (!function_exists('c9_client_setup')) {
 							)
 						),
 						'meta_custom' => array('meta', array(
-							'title'	=> __('WordPress Meta', 'c9-work'),
+							'title'	=> __('WordPress Meta', 'c9-starter'),
 						)),
 						'c9work_about' => array(
 							'text',
 							array(
-								'title'	=> __('About C9 Work Theme', 'c9-work'),
-								'text'	=> __('Be sure to activate the <strong>C9 Blocks Plugin</strong> during theme setup. The blocks plugin includes the custom WordPress blocks for tabs, toggles, and the responsive grid system that makes the theme look super duper.', 'c9-work'),
+								'title'	=> __('About C9 Starter', 'c9-starter'),
+								'text'	=> __('Be sure to activate the <strong>C9 Blocks Plugin</strong> during theme setup. The blocks plugin includes the custom WordPress blocks for tabs, toggles, and the responsive grid system that makes the theme look super duper.', 'c9-starter'),
 							)
 						)
 					),
 					'right-sidebar'	=> array(
 						'search',
-						'c9work_about' => array(
+						'c9starter_about' => array(
 							'text',
 							array(
-								'title'	=> 'About C9 Work Theme',
+								'title'	=> 'About C9 Starter',
 								'text'	=> 'Be sure to activate the <strong>C9 Blocks Plugin</strong> during theme setup. The blocks plugin includes the custom WordPress blocks for tabs, toggles, and the responsive grid system that makes the theme look super duper.'
 							)
 						),
@@ -180,10 +177,10 @@ if (!function_exists('c9_client_setup')) {
 					),
 					'left-sidebar'	=> array(
 						'search',
-						'c9work_about' => array(
+						'c9starter_about' => array(
 							'text',
 							array(
-								'title'	=> 'About C9 Work Theme',
+								'title'	=> 'About C9 Starter',
 								'text'	=> 'Be sure to activate the <strong>C9 Blocks Plugin</strong> during theme setup. The blocks plugin includes the custom WordPress blocks for tabs, toggles, and the responsive grid system that makes the theme look super duper.'
 							)
 						),
@@ -198,14 +195,6 @@ if (!function_exists('c9_client_setup')) {
 		/**
 		 * Editor color palette
 		 */
-// $primary: #000000;
-// $secondary: #333333;
-// $success: #21a77a;
-// $info: #f7f7f9;
-// $warning: #ec971f;
-// $danger: #843534;
-// $dark: #000000;
-// $light: #ffffff;
 		add_theme_support('editor-color-palette', array(
 			array(
 				'name' => 'primary',
@@ -273,6 +262,6 @@ if (!function_exists('c9_client_setup')) {
 // 	'core/separator',
 // 	array(
 // 		'name'         => 'black-bar',
-// 		'label'        => __('Full Width Black Bar', 'c9-work'),
+// 		'label'        => __('Full Width Black Bar', 'c9-starter'),
 // 	)
 // );
