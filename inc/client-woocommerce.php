13a076140d22d02fa2809a1785e8657fd59b1633
tim <tim@covertnine.com>
2020-10-23 15:37:08 -0500

    fixing anon function and made into function that can be unhooked

--- inc/client-woocommerce.php
+++ inc/client-woocommerce.php
@@ -1,17 +1,19 @@
 <?php
-
+//add WooCommerce support
 add_action('after_setup_theme', 'c9_add_woocommerce_support');
 if (! function_exists('c9_add_woocommerce_support')) {
 
-    function c9_add_woocommerce_support()
-    {
+    function c9_add_woocommerce_support() {
         add_theme_support('woocommerce');
     }
 }
 
 // $path defaults to 'woocommerce/' (in client folder)
-add_filter(
-    'woocommerce_template_path', function () {
+add_filter('woocommerce_template_path', 'c9_woocommerce_template_path');
+if (! function_exists('c9_woocommerce_template_path')) {
+
+	function c9_woocommerce_template_path() {
         return 'client/woocommerce/';
-    }
-);
+	}
+
+}
